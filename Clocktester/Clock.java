ed7b41a69606459906ad9f25bfba6f8aaa5c27c8
Varun-Pawar <45148561+Varun-Pawar@users.noreply.github.com>
2018-11-18 12:23:49 -0500

    Initial commit

---
+++ Clocktester/Clock.java
@@ -0,0 +1,45 @@
+package Clocktester
+
+public class Clock {
+	public static void main(String[] args) {
+ /*
+  * let x=seconds
+  * let y= minutes
+  * let z= hours
+  */
+	int seconds= 1 ;
+	int minutes= 1 ;
+	int hours= 1 ;  
+	}
+ int addseconds(int seconds) {
+	 /*
+	  * The input value must be greater than 0 but less than 60 {0<x<60} 
+	  * 35+40/60=    65/65=     1 remainder 5
+	  * more than 59 seconds- go back to 1 seconds, but add 1 minute
+	  *How many seconds are you adding      
+	  */
+	
+ }
+ 
+ int addminutes(int minutes) {
+	 /*
+	  * The input value must be greater than 0 but less than 60 {0<x<60}
+	  *How many minutes are you adding ? 
+	  *more than 59 minutes- go back to 1 minutes but add 1 hour
+      *  more
+	  */
+	
+ }
+	int addhours(int hours)
+	{
+		 /*
+		  * The input value must be greater than 0 but less than 24 {0<x<24}
+		  *How many hours are you adding ?
+		  * more than 23 hours, set hours minutes and seconds to zero (new day)
+		  */         
+		return 1;
+	}}
+	
+	
+			
+			
---
+++ Clocktester/package-info.java
@@ -0,0 +1,54 @@
+
+
+package Clocktester
+public class package-info.java {
+	public static void main(String[] args) {
+ /*
+  * let x=seconds;
+  * let y= minutes;
+  * let z= hours;
+  */
+	int seconds= 1 ;
+	int minutes= 0 ;
+	int hours= 0 ;  
+	}
+  if(int seconds==60);
+	 {
+	 /*
+	  * The input value must be greater than 0 but less than 60 {0<x<60} 
+	  * 35+40/60=    65/65=     1 remainder 5
+	  * more than 59 seconds- go back to 1 seconds, but add 1 minute
+	  *How many seconds are you adding      
+	  */
+  int seconds= 1;
+	 int minutes= int minutes + 1;
+else(int seconds+1);	
+ }
+
+ if(int minutes==60); {
+	 /*
+	  * The input value must be greater than 0 but less than 60 {0<x<60}
+	  *How many minutes are you adding ? 
+	  *more than 59 minutes- go back to 1 minutes but add 1 hour
+      *  more
+	  */
+	int minutes= 0;
+	int hours=inthours+1;
+	else(int minutes+1);
+
+ }
+	
+ if(int hours==24);	{
+		 /*
+		  * The input value must be greater than 0 but less than 24 {0<x<24}
+		  *How many hours are you adding ?
+		  * more than 23 hours, set hours minutes and seconds to zero (new day)
+		  */         
+	
+	int hours=0;
+	else(hours +1);
+	
+	}}
+	
+			
+			
